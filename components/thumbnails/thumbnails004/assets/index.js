91a6f87f6ada186bc0d1ab0bf469c5fef17fe46e
amarsgraph <mailto:amar@smoothgraph.in>
2022-04-12 15:35:08 +0530

    exhibition page error

--- components/thumbnails/thumbnails004/assets/index.js
+++ components/thumbnails/thumbnails004/assets/index.js
@@ -1,173 +1,49 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import classes from './Assets002.module.scss';
 
-import Thumbs from '../../../../public/profile/profile002/images/thumbs.png';
+import Thumb from '../../../../public/profile/profile004/images/thumb.png';
 
 export default function Assets002() {
       return (
 		<>
 			<div className={classes.assetsAll}>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
+				<div className={classes.thumblist}>
+					<div className={classes.thumbImg}>
+						<Link className={classes.thumbsItem} href="#">
+						<a>
+						<Image
+							src={Thumb}
+							height={513}
+							width={513}
+							alt="Thumbs"
+						/>
+						</a>							
 						</Link>
-						</article>
-
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<div className="w-100 d-none d-md-block"></div>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<div className="w-100 d-none d-md-block"></div>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
-						<article className={classes.thumbsWrapAll}>
-						<Link className={classes.thumbsItem} href="/">
-							<a>
-							<Image
-								src={Thumbs}
-								height={512}
-								width={512}
-								alt="Thumbs"
-							/>
-							</a>
-						</Link>
-						</article>
+						<div className={classes.wishLike}>
+						<FontAwesomeIcon icon={faHeart} className="wishicon"/>
+						</div>
+					</div>
+					<div className={classes.thumbInfo}>            
+						<div className={classes.thumbTitlewrap}>
+						<h3>
+						<Link href="/"><a>#3330</a></Link>
+						</h3>	
+						<div className={classes.assetNum}>32 Assets</div>						
+						</div>
+						<p className={classes.thumbDesc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisl malesuada id et a sed nulla. Dignissim augue venenatis non sed quis urna ac integer.</p>
+						<div className={classes.dateWish}>              
+						<span className={classes.date} >Last Updated: 12/12/22</span>
+						<div className={classes.wish}>
+							<span className={classes.num}>100 </span>
+							<FontAwesomeIcon icon={faHeart} className="wishicon"/>
+						</div>						
+						</div>		
+					</div>
+				</div>
 			</div>
 		</>
   );
